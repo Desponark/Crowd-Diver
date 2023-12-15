6166ec90a9a744c7a93409b6257b6dba338c0179
Desponark <129955348+Desponark@users.noreply.github.com>
2023-12-15 11:24:54 +0100

    Added empty C++ class in order to rebuild project solution so that Advanced Session Plugin hopefully works
    [git-p4: depot-paths = "//2022p3t5/mainline-projectx/ProjectX/": change = 20294]

---
+++ Source/ProjectX/MyClass.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * Empty class just so C++ code can be compiled for Advanced Sessions Plugin
+ */
+class PROJECTX_API MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+};
---
+++ Source/ProjectX/ProjectX.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ProjectX.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectX, "ProjectX" );
